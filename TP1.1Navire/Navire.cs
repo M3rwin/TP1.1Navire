f7917b9cbd4c474eb2154003e56deb42bac5af45
Marco CLIN <marcoetude@gmail.com>
2023-02-27 16:42:50 +0100

    Ajoutez des fichiers projet.

---
+++ TP1.1Navire/Navire.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TP1._1Navire
+{
+    class Navire
+    {
+        // PRIVATE ATTRIBUTES
+        private string imo;
+        private string nom;
+        private string libelleFret;
+        private int qteFretMaxi;
+
+
+        // CONSTRUCTOR
+        public Navire(string imo, string nom)
+        {
+            this.Imo = imo;
+            this.nom = nom;
+            this.libelleFret = "null";
+            this.QteFretMaxi = 0;
+        }
+
+        public Navire(string imo, string nom, string libelleFret, int qteFretMaxi)
+        {
+            this.Imo = imo;
+            this.nom = nom;
+            this.libelleFret = libelleFret;
+            this.QteFretMaxi = qteFretMaxi;
+        }
+        // SETTERS GETTERS
+        public string Imo
+        {
+            get => imo;
+            set
+            {
+                string pattern = @"^[I][M][O][0-9]{7}";
+                if (Regex.IsMatch(value, pattern))
+                {
+                    this.imo = value;
+                }
+                else
+                {
+                    throw new Exception("Erreur : IMO invalide");
+                }
+            }
+        }
+        public string Nom { get => nom; set => nom = value; }
+        public string LibelleFret { get => libelleFret; set => libelleFret = value; }
+        public int QteFretMaxi
+        {
+            get => qteFretMaxi;
+            set
+            {
+                if(value >= 0)
+                {
+                    this.qteFretMaxi = value;
+                }
+                else
+                {
+                    throw new Exception("Erreur, quantité de fret non valide");
+                }
+            }
+        }
+
+
+        //METHODES
+    }
+}
---
+++ TP1.1Navire/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TP1._1Navire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ExpressionReguliere();
+                Console.WriteLine("Fin normale du programme");
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+
+
+        private static void Instanciations()
+        {
+            try
+            {
+                Navire navire = new Navire("IMO0000001", "Bateau1", "Cargaison1", 65412);
+                navire = new Navire("IMO0000002", "Bateau2", "Cargaison1", 123684);
+                navire = new Navire("IMO0000003", "Bateau3", "Cargaison1", -56);
+                navire = new Navire("IMO0000004", "Bateau4", "Cargaison1", 8546);
+                navire = new Navire("IMO0000005", "Bateau5", "Cargaison1", 6541462);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void ExpressionReguliere()
+        {
+            Navire navire = new Navire("IMO0000001", "Bateau1");
+            Console.WriteLine($"Premier bateau enregistré, IMO : {navire.Imo}, Nom : {navire.Nom}, Fret : {navire.LibelleFret}, Qte : {navire.QteFretMaxi}");
+            
+            navire = new Navire("IMO0000002", "Bateau2", "Cargaison1", 123684);
+            Console.WriteLine($"Deuxième bateau enregistré, IMO : {navire.Imo}, Nom : {navire.Nom}, Fret : {navire.LibelleFret}, Qte : {navire.QteFretMaxi}");
+            navire = new Navire("IMO0000003", "Bateau3", "Cargaison1", 56);
+            navire = new Navire("IMO0000004", "Bateau4", "Cargaison1", 8546);
+            navire = new Navire("IMO0000005", "Bateau5", "Cargaison1", 6541462);
+        }
+    }
+}
