0c3a3d323a1ff91f5571069e8c5937ecb0cc32ea
M3rwin <marcoetude@gmail.com>
2023-02-28 21:57:43 +0100

    retrait code inutile

--- TP1.1Navire/Port.cs
+++ TP1.1Navire/Port.cs
@@ -47,30 +47,6 @@ namespace TP1._1Navire
         }
 
 
-        //public int RecupPosition(string imo)
-        //{
-        //    for (int i = 0; i < this.navires.Count; i++)
-        //    {
-        //        if (this.navires[i].Imo == imo)
-        //        {
-        //            return i;
-        //        }
-        //    }
-        //    return -1;
-        //}
-
-        //public int RecupPosition(Navire navire)
-        //{
-        //    if (this.navires.Contains(navire))
-        //    {
-        //        return this.navires.IndexOf(navire);
-        //    }
-        //    else
-        //    {
-        //        return -1;
-        //    }
-        //}
-
         public void EnregistrerDepart(string imo)
         {
             if (this.EstPresent(imo))
@@ -82,39 +58,6 @@ namespace TP1._1Navire
                 throw new GestionPortException("Le navire spécifié n'est pas présent dans le port");
             }
         }
-
-
-        //public void TestRecupPosition()
-        //{
-        //    Navire test1 = new Navire("IMO1564879", "SS Test1");
-        //    Navire test2 = new Navire("IMO1564880", "SS Test2");
-        //    Navire test3 = new Navire("IMO1564881", "SS Test3");
-        //    Navire test4 = new Navire("IMO1564882", "SS Test4");
-
-        //    this.EnregistrerArrivee(test1);
-        //    this.EnregistrerArrivee(test2);
-        //    this.EnregistrerArrivee(test3);
-
-        //    Console.WriteLine("Indice du SS Test1 (" + test1.Imo + ") : " + this.RecupPosition(test1.Imo));
-        //    Console.WriteLine("Indice du SS Test2 (" + test3.Imo + ") : " + this.RecupPosition(test3.Imo));
-        //    Console.WriteLine("Indice du SS Test4 (" + test4.Imo + ") : " + this.RecupPosition(test4.Imo));
-        //}
-
-        //public void TestRecupPositionV2()
-        //{
-        //    Navire test1 = new Navire("IMO1564879", "SS Test1");
-        //    Navire test2 = new Navire("IMO1564880", "SS Test2");
-        //    Navire test3 = new Navire("IMO1564881", "SS Test3");
-        //    Navire test4 = new Navire("IMO1564882", "SS Test4");
-
-        //    this.EnregistrerArrivee(test1);
-        //    this.EnregistrerArrivee(test2);
-        //    this.EnregistrerArrivee(test3);
-
-        //    Console.WriteLine("Indice du SS Test1 (" + test1.Imo + ") : " + this.RecupPosition(test1));
-        //    Console.WriteLine("Indice du SS Test2 (" + test3.Imo + ") : " + this.RecupPosition(test3));
-        //    Console.WriteLine("Indice du SS Test4 (" + test4.Imo + ") : " + this.RecupPosition(test4));
-        //}
 
 
         public void Dechargement(string imo)
